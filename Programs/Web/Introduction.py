68c889893d0d45e66d380253816f074e4f6d254d
Mohanrajbeece <mohanrajbeece@gmail.com>
2023-01-17 08:59:54 +0530

    Introduction

---
+++ Programs/Web/Introduction.py
@@ -0,0 +1,26 @@
+print("Mohan")
+print("Mohan")
+def add():
+    num1=10
+    num2=70
+    Num3=num1+num2
+    return Num3
+
+
+def printtext():
+    print("Python learning begins with besant")
+    print("day3 Tranings")
+
+
+print (add())
+printtext()
+
+def addresult(num4,num5):
+    newnum=num4 + num5 
+    return newnum
+
+Result1=addresult(2300,4500)
+print(Result1)
+Result2=addresult(7400,4500)
+print(Result2)
+print(addresult(4000,5000))
