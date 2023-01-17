b5298c96f097863038b00592d19c78d55526c757
Mohanrajbeece <mohanrajbeece@gmail.com>
2023-01-17 21:31:24 +0530

    sysrepeat

---
+++ Programs/Web/sysrepeat.py
@@ -0,0 +1,14 @@
+import sys
+
+def printtext(name):
+    print("Hello" , name , sep="-")
+    print("bye" , name)
+    
+if __name__ == '__main__':
+     printtext(sys.argv[1])
+     printtext(sys.argv[2])
+     printtext(sys.argv[3])
+
+
+ 
+     
